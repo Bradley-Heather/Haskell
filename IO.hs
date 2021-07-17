0b8bb68664e12558ffd801ab11bed75862970f1a
Bradley Heather <80346526+Bradley-Heather@users.noreply.github.com>
2021-07-17 15:31:51 -0400

    Create IO.hs

---
+++ IO.hs
@@ -0,0 +1,63 @@
+module IO where
+
+
+import     Control.Monad    (liftM, liftM2)
+import     Data.Char        (toUpper)
+import     System.Directory
+import     System.IO        (Handle, IOMode (ReadMode), hGetLine, hIsEOF, withFile)
+import     System.IO.Error  (catchIOError) 
+
+getTwoLines :: IO String 
+getTwoLines = getLine >> getLine 
+
+getTwoLines' :: IO String 
+getTwoLines' = do
+    getLine 
+    getLine 
+
+duplicateLine :: IO String 
+duplicateLine = fmap (\x -> x ++ x) getLine 
+
+shout :: IO String
+shout = fmap (map toUpper) getLine 
+
+joinTwoLines :: IO String
+joinTwoLines = liftM2 (++) getLine getLine 
+
+flipTwoLines :: IO String
+flipTwoLines = liftM2 (flip (++)) getLine getLine 
+
+{- Bind  (>>=) :: IO a -> (a -> IOb) -> IO b -}
+
+shoutBack :: IO()
+shoutBack = shout >>= putStrLn 
+
+shoutBackTwice :: IO()
+shoutBackTwice = 
+    shout >>= \x -> putStrLn x >> putStrLn x 
+
+liftM2' :: (a -> b -> c) -> IO a -> IO b -> IO c
+liftM2' f ioa iob =
+    ioa  >>= \a ->
+    iob  >>= \b -> 
+    return (f a b)
+
+liftM2'' :: (a -> b -> c) -> IO a -> IO b -> IO c
+liftM2'' f ioa iob = do
+    a <- ioa
+    f a <$> iob
+    
+    
+greeting :: IO ()
+greeting = do  
+     putStrLn "What is your name?"  
+     name <- getLine 
+     putStrLn ("Hello, " ++ name ++ "!")
+     putStrLn "Where do you live?"
+     loc <- getLine
+     let
+       answer
+         | loc == "South Africa" = "Brilliant!"
+         | otherwise             = "Not bad..."
+     putStrLn answer
+
