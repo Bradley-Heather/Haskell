91cb72bc61e88f40228d10ac6de5ef2978c78866
Bradley Heather <80346526+Bradley-Heather@users.noreply.github.com>
2021-07-18 18:19:23 -0400

    Update Lockercheck.hs

--- Lockercheck.hs
+++ Lockercheck.hs
@@ -1,4 +1,10 @@
+
 import qualified Data.Map as Map
+import           Control.Monad    (liftM, liftM2, mapM)
+import           Data.Char        (toUpper)
+import           System.Directory ()
+import           System.IO        (Handle, IOMode (ReadMode), hGetLine, hIsEOF, withFile)
+import           System.IO.Error  (catchIOError) 
 
 data LockerState = Taken | Free 
   deriving (Show, Eq)
@@ -26,4 +32,30 @@ lockers = Map.fromList
     ,(104, (Taken, "rn763"))
     ]
 
-addLocker
+data Interaction = 
+    Question String Interaction Interaction
+    | Result String
+    deriving (Show, Eq) 
+
+askBool :: String -> IO Bool 
+askBool question = do
+    putStrLn (question ++ "Y/N")
+    x <- getChar 
+    putStrLn ""
+    return (x `elem` "Yy")
+
+interaction :: Interaction -> IO ()
+interaction (Question q y n) = do
+    b <- askBool q
+    if b then interaction y else interaction n
+interaction (Result r) = putStrLn r     
+
+
+addLocker :: Interaction
+addLocker =
+    Question "Would you like to add a locker?"
+         (Question "What will it's code be?")
+            Result "Great, I've added it"
+         (Result "No problem.")
+
+
