8f7c386504220631559126163bf0556e1488d522
Bradley Heather <80346526+Bradley-Heather@users.noreply.github.com>
2021-08-03 11:22:10 -0400

    Update AwesomeNumber.hs

--- AwesomeNumber.hs
+++ AwesomeNumber.hs
@@ -6,9 +6,9 @@ data Answer = No | Almost | Yes deriving (Show, Read, Eq, Ord)
 
 isInteresting :: Integer -> [Integer] -> Answer
 isInteresting a as
-    | interestingNum a as                                 = Yes
+    | interestingNum a as                                    = Yes
     | interestingNum (a + 1) as || interestingNum (a + 2) as = Almost
-    | otherwise                                                = No
+    | otherwise                                              = No
 
 interestingNum :: Integer -> [Integer] -> Bool
 interestingNum x xs = x > 99 && (x `rem` (10 ^ (length (show x) - 1)) == 0 || show x == reverse (show x) || isAscending x || isDescending x || x `elem` xs)
@@ -34,3 +34,27 @@ isAscending n = correctCount' $ count n
 
 isDescending :: Integer -> Bool
 isDescending n = correctCount $ reverse (count n)
+
+-- alternate simplified solution
+
+{-
+{-# LANGUAGE ViewPatterns #-}
+module Awesome.Numbers where
+import Data.List
+
+data Answer = No | Almost | Yes deriving (Show, Read, Eq, Ord)
+
+isInteresting :: Integer -> [Integer] -> Answer
+isInteresting x xs | isMileage x                        = Yes
+                   | isMileage (x+1) || isMileage (x+2) = Almost
+                   | otherwise                          = No
+  where isMileage (show -> y) = (length y >= 3) && any ($y) 
+               [ all (== '0') . tail
+               , all (== head y)
+               , (`isInfixOf` "1234567890")
+               , (`isInfixOf` "9876543210")
+               , (== (reverse y))
+               , (`elem` xs) . read
+               ]
+
+-}
