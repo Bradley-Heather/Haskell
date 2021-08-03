d48c6d96aa3519f0fa2167f7011e0edc498c515f
Bradley Heather <80346526+Bradley-Heather@users.noreply.github.com>
2021-08-03 09:42:15 -0400

    Update AwesomeNumber.hs

--- AwesomeNumber.hs
+++ AwesomeNumber.hs
@@ -1,6 +1,29 @@
 module Awesome.Numbers where
 
+import Data.Char
+
 data Answer = No | Almost | Yes deriving (Show, Read, Eq, Ord)
 
-isInteresting :: Integer -> [Integer] -> Answer
-isInteresting x xs = error "todo: isInteresting"
+
+isInteristing x =
+    if isInteresting' x       == True  then  Yes
+  --  if isInteresting' (x + 1) == True || isInteresting' (x + 2) == True then Almost
+    else No
+
+isInteresting' x = x `rem` (10 ^ (length (show x) - 1)) == 0 || show x == reverse (show x) || isAscending x || isDescending x 
+  
+
+count :: Int -> [Int]
+count n = map digitToInt $ show n 
+
+correctCount :: [Int] -> Bool
+correctCount [x] = True
+correctCount (x:n:ns) =
+    if x + 1 == n then correctCount (n:ns)
+    else False
+
+isAscending :: Int -> Bool
+isAscending n = correctCount $ count n
+
+isDescending :: Int -> Bool
+isDescending n = correctCount $ reverse (count n)
